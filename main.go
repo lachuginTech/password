dc2eb820571ddd0a1fd5e2b4ca4de5820c529813
lachuginTech <vladimir.lachugin.tech@gmail.com>
2024-11-09 20:52:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
-	
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	myAccount := account{login, password, url}
+
+	outputPassword(&myAccount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var input string
+	fmt.Scan(&input)
+	return input
+}
+
+func outputPassword(acc *account) {
+	fmt.Println(acc.login, acc.password, acc.url)
 }
